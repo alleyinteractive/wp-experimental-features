e55152705f3635678d7ee4f3bd0339aebe5cffa1
kevinfodness <kevin@kevinfodness.com>
2020-11-05 15:10:50 -0500

    Add tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Initialize tests for this plugin.
+ *
+ * @package Experimental_Features
+ */
+
+// Load Core's test suite.
+$experimental_features_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $experimental_features_tests_dir ) {
+	$experimental_features_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+require_once $experimental_features_tests_dir . '/includes/functions.php'; // phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable
+
+/**
+ * Setup our environment.
+ */
+function experimental_features_manually_load_environment() {
+	/*
+	 * Tests won't start until the uploads directory is scanned, so use the
+	 * lightweight directory from the test install.
+	 *
+	 * @see https://core.trac.wordpress.org/changeset/29120.
+	 */
+	add_filter(
+		'pre_option_upload_path',
+		function () {
+			return ABSPATH . 'wp-content/uploads';
+		}
+	);
+
+	// Load this plugin.
+	require_once dirname( __DIR__ ) . '/wp-experimental-features.php';
+}
+tests_add_filter( 'muplugins_loaded', 'experimental_features_manually_load_environment' );
+
+// Include core's bootstrap.
+require $experimental_features_tests_dir . '/includes/bootstrap.php'; // phpcs:ignore WordPressVIPMinimum.Files.IncludingFile.UsingVariable
